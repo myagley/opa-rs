07fa0eef87208e76c1ad7f6b96eb11e32b62605a
Mike Yagley <myagley@gmail.com>
2020-04-10 20:40:44 -0700

    Add cross compilation for armv7

--- opa-compiler-sys/build.rs
+++ opa-compiler-sys/build.rs
@@ -2,8 +2,19 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    println!("cargo:rerun-if-changed={}", "opa.go");
-    gobuild::Build::new().file("opa.go").compile("opa");
+    let root = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    let file = root.join("opa.go");
+    let mut go = gobuild::Build::new();
+    go.file(&file);
+
+    let target_arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap();
+    if target_arch == "arm" {
+        go.env("CC", "arm-linux-gnueabihf-gcc");
+        go.env("GOOS", "linux");
+        go.env("GOARCH", "arm");
+    }
+    go.env("CGO_ENABLED", "1");
+    go.compile("opa");
 
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     let header = out_path.join("libopa.h");
@@ -12,6 +23,7 @@ fn main() {
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
         .whitelist_function("Build")
         .whitelist_function("Free")
+        .clang_arg("-I/usr/arm-linux-gnueabihf/include")
         .generate()
         .expect("Unable to generate bindings");
 
--- opa-compiler-sys/src/lib.rs
+++ opa-compiler-sys/src/lib.rs
@@ -3,11 +3,3 @@
 #![allow(non_snake_case)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
